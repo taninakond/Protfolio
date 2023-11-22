a68ab0c5b8eb15e10ef4776151ffc81cb139c949
Tanin Ahmed <taninakond@gmail.com>
2023-11-22 23:29:42 +0600

    some update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    build: {
+        manifest: true,
+        minify: false,
+        outDir: 'build', // Specify the output directory for the build
+        rollupOptions: {
+            input: {
+                main: './src/main.jsx', // Specify the entry point
+            },
+            output: {
+                entryFileNames: 'main.js',
+                assetFileNames: 'assets/[name][extname]',
+                format: 'iife',
+            },
+        },
+    },
+});
